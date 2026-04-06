74896800e44bc49fb526b4ee307d6829dae7ec93
Gxstavo-dev <gps5821s@outlook.com>
2026-04-05 23:03:38 -0600

    docs: rescribir funciones de py a go para exponerlas en el frontend

--- main.go
+++ main.go
@@ -28,29 +28,21 @@ func crearTablas(db *sql.DB) error {
 
 }
 
-func crearNota(db *sql.DB) {
-
-}
-func actualizarNota(db *sql.DB) {
-
-}
-func mostrarNota(db *sql.DB) {
-
-}
-func mostrarNotas(db *sql.DB) {
-
-}
-func eliminarNota(db *sql.DB) {
-
+// tipar los datos de Nota { lo de json es para retornarlo en js }
+type Nota struct {
+	Id             int64  `json:"id"`
+	Titulo         string `json:"titulo"`
+	Contenido      string `json:"contenido"`
+	UltimoGuardado string `json:"ultimoGuardado"`
 }
 
 func main() {
 
 	// verificamos si "templates/index.html" es una ruta relativa si es asi almacenamos
 	ruta, err := filepath.Abs("templates/index.html")
-	// si es error detiene la app
+
 	if err != nil {
-		panic(err)
+		panic(err) // si es error detiene la app
 	}
 
 	w := webview.New(true) // true = debug/DevTools habilitado
@@ -58,7 +50,7 @@ func main() {
 	w.SetTitle("Void Notas")
 	w.SetSize(1200, 700, webview.HintFixed) // evitar resizable
 
-	db, err := sql.Open("sqlite", "./Database/app.db")
+	db, err := sql.Open("sqlite", "./Database/app.db") //  conectarse  a la db
 
 	// error al abrir la db
 	if err != nil {
@@ -70,11 +62,108 @@ func main() {
 		log.Fatal("Error creando tablas:", err)
 	}
 
-	w.Bind("crearNota", crearNota(db))
-	w.Bind("actualizarNota", actualizarNota(db))
-	w.Bind("mostrarNota", mostrarNota(db))
-	w.Bind("mostrarNotas", mostrarNotas(db))
-	w.Bind("eliminarNota", eliminarNota(db))
+	// binding para crear notas la cual tendra como retorno Nota
+	w.Bind("crearNota", func(titulo, contenido, ultimoGuardado string) Nota {
+
+		// ejecutar consulta
+		query, err := db.Exec(
+			"INSERT INTO notas (titulo, contenido, ultimoGuardado) VALUES (?, ?, ?)",
+			titulo, contenido, ultimoGuardado,
+		)
+
+		if err != nil {
+			log.Printf("error al insertar nota: %v", err)
+			return Nota{} // retornamos nota vacia
+		}
+		id, err := query.LastInsertId() // obtener el id de esa nota creada
+
+		if err != nil {
+			log.Printf("error obteniendo id: %v", err)
+			return Nota{}
+		}
+
+		// retornamos todos los datos
+		return Nota{
+			Id:             id,
+			Titulo:         titulo,
+			Contenido:      contenido,
+			UltimoGuardado: ultimoGuardado,
+		}
+	})
+
+	// binding para actualizar la nota y retornara true o false
+	w.Bind("actualizarNota", func(id int, titulo, contenido, ultimoGuardado string) bool {
+		_, err := db.Exec("Update notas SET titulo = ? , contenido = ? , ultimoGuardado = ? WHERE id = ?", titulo, contenido, ultimoGuardado, id)
+
+		if err != nil {
+			log.Printf("error al actualizar la nota: %v", err)
+			return false // si fall retorna false
+		}
+
+		return true
+	})
+
+	// binding mostrarnota que solo necesita el id como parametro
+
+	w.Bind("mostrarNota", func(id int) Nota {
+
+		// guardamos la la structuc nota vacia en nueva variable
+		nota := Nota{}
+
+		// para leer una fila
+		err := db.QueryRow(
+			"SELECT id, titulo, contenido, ultimoGuardado FROM notas WHERE id = ?", id,
+		).Scan(&nota.Id, &nota.Titulo, &nota.Contenido, &nota.UltimoGuardado)
+		// .Scan() mapea cada columna del SELECT a cada campo del struct, en orden
+
+		// si no existe la nota retorna nada
+		if err == sql.ErrNoRows {
+			log.Printf("nota %d no existe", id)
+			return Nota{}
+		}
+
+		if err != nil {
+			log.Printf("error al mostrar nota id %d: %v", id, err)
+			return Nota{}
+		}
+
+		return nota // retorna los datos de guardamos en nota del struct Nota
+	})
+
+	// binding mostrarnotas para retornar todas las notas de la db
+	w.Bind("mostrarNotas", func() []Nota {
+		rows, err := db.Query("SELECT id, titulo, contenido, ultimoGuardado FROM notas")
+
+		if err != nil {
+			log.Printf("error al listar notas: %v", err)
+			return []Nota{}
+		}
+		defer rows.Close() // cerrar la conexion al terminar la funcion ( uso de defer para no tener que estar cerrando la db  para liberar la memoria)
+
+		notas := []Nota{} // crear un array de la struct notas vacio
+
+		for rows.Next() { // uso de next + for para recorrer cada fila y si existe ese fila
+			var n Nota
+			// leer los datos de la fila actual
+			if err := rows.Scan(&n.Id, &n.Titulo, &n.Contenido, &n.UltimoGuardado); err != nil {
+				continue
+			}
+			notas = append(notas, n) //  crear un slice vacio de Nota
+		}
+		return notas
+	})
+
+	// binding eliminar nota que retorna true o false
+	w.Bind("eliminarNota", func(id int64) bool {
+
+		_, err := db.Exec(" DELETE FROM notas WHERE id = ?;", id)
+
+		if err != nil {
+			log.Printf("error al eliminar las notas: %v", err)
+			return false
+		}
+		return true
+	})
 
 	// para que en linux funcione la ruta utilzar file://
 	w.Navigate("file://" + ruta)
